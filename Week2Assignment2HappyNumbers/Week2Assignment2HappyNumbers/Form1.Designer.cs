b23e686e0d7b092907c654515591ec3aca101daa
gulledgepo <gulledgepo@gmail.com>
2017-09-05 11:44:54 -0700

    Initial Commit

---
+++ Week2Assignment2HappyNumbers/Week2Assignment2HappyNumbers/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Week2Assignment2HappyNumbers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.btnHappyList = new System.Windows.Forms.Button();
+            this.lblHappyList = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(80, 12);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(100, 20);
+            this.txtNumber.TabIndex = 0;
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(49, 49);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(159, 23);
+            this.btnTest.TabIndex = 1;
+            this.btnTest.Text = "Test for Happy Number";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // btnHappyList
+            // 
+            this.btnHappyList.Location = new System.Drawing.Point(49, 137);
+            this.btnHappyList.Name = "btnHappyList";
+            this.btnHappyList.Size = new System.Drawing.Size(159, 23);
+            this.btnHappyList.TabIndex = 2;
+            this.btnHappyList.Text = "List First 8 Happy Numbers";
+            this.btnHappyList.UseVisualStyleBackColor = true;
+            this.btnHappyList.Click += new System.EventHandler(this.btnHappyList_Click);
+            // 
+            // lblHappyList
+            // 
+            this.lblHappyList.AutoSize = true;
+            this.lblHappyList.Location = new System.Drawing.Point(46, 100);
+            this.lblHappyList.Name = "lblHappyList";
+            this.lblHappyList.Size = new System.Drawing.Size(0, 13);
+            this.lblHappyList.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(261, 185);
+            this.Controls.Add(this.lblHappyList);
+            this.Controls.Add(this.btnHappyList);
+            this.Controls.Add(this.btnTest);
+            this.Controls.Add(this.txtNumber);
+            this.Name = "Form1";
+            this.Text = "Happy Numbers";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.Button btnHappyList;
+        private System.Windows.Forms.Label lblHappyList;
+    }
+}
+
---
+++ Week2Assignment2HappyNumbers/Week2Assignment2HappyNumbers/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Week2Assignment2HappyNumbers
+{
+    //Paxston Gulledge
+
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            int number;
+            bool success = Int32.TryParse(txtNumber.Text, out number);
+            bool unhappy = false;
+            if (number > 0 && success == true)
+            {
+                unhappy = IsNumberHappy(number);
+                if (unhappy == false)
+                {
+                    MessageBox.Show(txtNumber.Text + " is a happy number.");
+                }
+                else
+                {
+                    MessageBox.Show(txtNumber.Text + " is an unhappy number.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter a positive number greater than 0.");
+            }
+
+        }
+        private bool IsNumberHappy(int number)
+        {
+            int digit;
+            int sum = 0;
+            bool unhappy = false;
+            while (number != 1 && unhappy == false)
+            {
+                while (number != 0 && unhappy == false)
+                {
+                    digit = number % 10;
+                    sum += digit * digit;
+                    number = number / 10;
+                }
+                number = sum;
+                if (number == 4)
+                {
+                    unhappy = true;
+                    return true;
+                }
+                sum = 0;
+            }
+            return false;
+        }
+
+        private void btnHappyList_Click(object sender, EventArgs e)
+        {
+            bool unhappy = false;
+            string happyList = "1";
+            for (int i = 2; i < 32; i++)
+            {
+                unhappy = IsNumberHappy(i);
+                if (unhappy == false)
+                {
+                    happyList = happyList + ", " + Convert.ToString(i);
+                }
+            }
+            lblHappyList.Text = happyList;
+        }
+    }
+}
